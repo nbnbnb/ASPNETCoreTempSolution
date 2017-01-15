fcdf55b2f510cf6a8cb967ab86d6c3ad6f3ce68a
ZhangJin <nbnbnb.zhang@gmail.com>
2017-01-15 17:53:15 +0800

    custom validator

---
+++ BasicSite/Content/Scripts/custom-validators.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+    jQuery.validator.addMethod('classicmovie',
+        function (value, element, params) {
+            // Get element value. Classic genre has value '0'.
+            var genre = $(params[0]).val(),
+                year = params[1],
+                date = new Date(value);
+            if (genre && genre.length > 0 && genre[0] === '0') {
+                // Since this is a classic movie, invalid if release date is after given year.
+                return date.getFullYear() <= year;
+            }
+
+            return true;
+        });
+
+    jQuery.validator.unobtrusive.adapters.add('classicmovie',
+        ['element', 'year'],
+        function (options) {
+            var element = $(options.form).find('select#Genre')[0];
+            options.rules['classicmovie'] = [element, parseInt(options.params['year'])];
+            options.messages['classicmovie'] = options.message;
+        });
+} (jQuery));
