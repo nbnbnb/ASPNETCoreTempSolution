ce382f30b7bf57f4a0883ca5804ff7a86cc9555b
张进 <jinzhanga@Ctrip.com>
2017-01-24 15:09:31 +0800

    Add TagHelpers

---
+++ BasicSite/Helpers/TagHelpers/AutoLinkerHttpTagHelper.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BasicSite.Helpers.TagHelpers
+{
+    [HtmlTargetElement("p", Attributes = "www")]
+    public class AutoLinkerHttpTagHelper : TagHelper
+    {
+        /// <summary>
+        /// 指定执行顺序
+        /// 数字越小先执行
+        /// 默认值为 0
+        /// </summary>
+        public override int Order
+        {
+            get { return int.MinValue; }
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            // This filter must run before the AutoLinkerWwwTagHelper as it searches and replaces http and 
+            // the AutoLinkerWwwTagHelper adds http to the markup.
+
+            // 多次执行 output.GetChildContentAsync() 将会获得相同的内容
+            // 可以使用 output.GetChildContentAsync(false) 获得最新内容，而不是缓存数据
+            // 由于其它的处理程序也会对内容进行修改
+            // 此处获取最新数据
+            var childContent = output.Content.IsModified ? output.Content.GetContent() :
+                    (await output.GetChildContentAsync()).GetContent();
+
+            // Find Urls in the content and replace them with their anchor tag equivalent.
+            output.Content.SetHtmlContent(Regex.Replace(
+                 childContent,
+                     @"\b(?:https?://)(\S+)\b",
+                      "<a target=\"_blank\" href=\"$0\">$0</a>"));  // http link version}
+        }
+    }
+}
---
+++ BasicSite/Helpers/TagHelpers/WebsiteInformationTagHelper.cs
@@ -0,0 +1,43 @@
+﻿using BasicSite.Models;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicSite.Helpers.TagHelpers
+{
+    /// <summary>
+    /// 约定将会进行 Pascal-cased 方式进行转换 http://c2.com/cgi/wiki?KebabCase  lower kebab case
+    /// 此处转换为 website-information
+    /// </summary>
+    public class WebsiteInformationTagHelper : TagHelper
+    {
+        /// <summary>
+        /// 标签中添加 info 属性，不区分大小写
+        /// 例如或者绑定一个视图对象
+        /* <website-information info="new WebsiteContext {
+                                            Version = new Version(1, 3),
+                                            CopyrightYear = 1638,
+                                            Approved = true,
+                                            TagsToShow = 131 }" />
+                                            **/
+        /// </summary>
+        public WebsiteContext Info { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "section";
+
+            output.Content.SetHtmlContent(
+               $@"<ul><li><strong>Version:</strong> {Info.Version}</li>
+                <li><strong>Copyright Year:</strong> {Info.CopyrightYear}</li>
+                <li><strong>Approved:</strong> {Info.Approved}</li>
+                <li><strong>Number of tags to show:</strong> {Info.TagsToShow}</li></ul>");
+
+            // 表示可以使用 <website-information></website-information> 和 <website-information/> 模式
+            // 如果不指定，则表示只能使用 <website-information></website-information> 模式
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
