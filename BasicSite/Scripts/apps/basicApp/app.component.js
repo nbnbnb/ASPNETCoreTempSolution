2b1424167a264e65cf6720818dd5e0320da75c8c
张进 <jinzhanga@Ctrip.com>
2017-05-11 14:07:24 +0800

    ES2015 + Angular4.1.1

--- BasicSite/Scripts/apps/basicApp/app.component.js
+++ BasicSite/Scripts/apps/basicApp/app.component.js
@@ -6,17 +6,16 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var core_1 = require("@angular/core");
-var AppComponent = (function () {
-    function AppComponent() {
+const core_1 = require("@angular/core");
+let AppComponent = class AppComponent {
+    constructor() {
         this.name = 'Angular';
     }
-    return AppComponent;
-}());
+};
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
+        template: `<h1>Hello {{name}}</h1>`,
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- BasicSite/Scripts/apps/knockoutApp/hello.js
+++ BasicSite/Scripts/apps/knockoutApp/hello.js
@@ -1,12 +1,11 @@
 ﻿"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var ko = require("knockout");
-var HelloViewModel = (function () {
-    function HelloViewModel(language, framework) {
+const ko = require("knockout");
+class HelloViewModel {
+    constructor(language, framework) {
         this.language = ko.observable(language);
         this.framework = ko.observable(framework);
     }
-    return HelloViewModel;
-}());
+}
 ko.applyBindings(new HelloViewModel("TypeScript", "Knockout"));
 //# sourceMappingURL=hello.js.map
